03cf5e73ba9616357a7507b46cd54518e5b43e4e
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-08-16 09:54:17 +0200

    Litt meir testing for å verifisere at api server fungerer som den skal.

--- src/test/kotlin/no/nav/k9/DiagnosekodeApiTest.kt
+++ src/test/kotlin/no/nav/k9/DiagnosekodeApiTest.kt
@@ -2,6 +2,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import no.nav.k9.DiagnosekodeApi
 import org.junit.Test
 
@@ -9,8 +10,10 @@ class DiagnosekodeApiTest {
 
     @Test
     fun testRequests() = withTestApplication(Application::DiagnosekodeApi) {
-        with(handleRequest(HttpMethod.Get, "/diagnosekoder?query=luftv&max=")) {
+        with(handleRequest(HttpMethod.Get, "/diagnosekoder?query=E614&max=")) {
             assertEquals(HttpStatusCode.OK, response.status())
+            assertTrue(response.contentType().match("application/json"))
+            assertEquals("""[{"kode":"E614","beskrivelse":"Krommangel"}]""", response.content)
         }
     }
 }
